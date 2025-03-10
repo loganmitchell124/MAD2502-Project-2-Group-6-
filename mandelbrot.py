3f5487c368bab794c2274bb6a1047ff953c06254
Santiago Otoya <s.otoya@ufl.edu>
2025-03-10 18:37:46 -0400

    get_complex_grid function

--- mandelbrot.py
+++ mandelbrot.py
@@ -23,3 +23,26 @@ print(get_escape_time(0.38+0.25j, 100))
 
 #outputs: 0, 1 , none, ****none****, 56
 
+def get_complex_grid(top_left: complex, bottom_right: complex, step: float
+) -> np.ndarray:
+    """
+        Author: Santiago Otoya
+        This function will compute a grid of complex numbers representing a grid for the Mandelbrot set
+        Params:
+           top_left: The top left corner of the grid
+           bottom_right: The bottom right corner of the grid
+           step: The step size of the grid
+        Return: np.ndarray
+    """
+    #Using bounds for real and imaginary values and generating their values
+    real_vals = np.arange(top_left.real, bottom_right.real, step)
+    imag_vals = np.arange(top_left.imag, bottom_right.imag, -step)
+
+    #Creating the 2d grid for each
+    grid_real_vals = np.meshgrid(real_vals, imag_vals)[0]
+    grid_imag_vals = np.meshgrid(real_vals, imag_vals)[1]
+
+    #Creating final complex grid by combining both grids
+    final_complex_grid = 1j * grid_imag_vals + grid_real_vals
+
+    return final_complex_grid
